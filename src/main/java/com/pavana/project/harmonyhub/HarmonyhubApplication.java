eac20a40546220a4770ebc6b9c364b9fc39e7f11
Pavana345 <82650697+Pavana345@users.noreply.github.com>
2023-12-28 16:51:01 +0530

    initial commit

---
+++ src/main/java/com/pavana/project/harmonyhub/HarmonyhubApplication.java
@@ -0,0 +1,13 @@
+package com.pavana.project.harmonyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavana/project/harmonyhub/HarmonyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavana.project.harmonyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
